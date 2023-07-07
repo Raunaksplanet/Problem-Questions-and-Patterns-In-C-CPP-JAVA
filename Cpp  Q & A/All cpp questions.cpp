f9586b158023943e8634fad9b2a2695d057da907
Raunak Gupta <111748076+Raunaksplanet@users.noreply.github.com>
2023-07-07 18:29:34 +0530

    Update All cpp questions.cpp

--- Cpp  Q & A/All cpp questions.cpp
+++ Cpp  Q & A/All cpp questions.cpp
@@ -1,5 +1,5 @@
 Q1.What will be the output of the following code.
-------------------------------------------------
+""""""""""""""""""""""""""""""""""""""""""""""""
 #include <iostream>
 using namespace std;
 
@@ -9,8 +9,18 @@ int main()
     int a = printf("Abhishek");
     cout << a;
 }
-------------------------------------------------
+""""""""""""""""""""""""""""""""""""""""""""""""
 Ans1. Abhishek8 
 --------------------------------------------------------------------------------------------------------------------------------------
-Q2.Write a program to Count the digits entered by user.
+Q2.if n = 45954 then print the mid value(Eg = 9) in a single line of code.
 ans2. 
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 45954;
+    cout << (a/100)%10;
+}
+--------------------------------------------------------------------------------------------------------------------------------------
+Q3. 
