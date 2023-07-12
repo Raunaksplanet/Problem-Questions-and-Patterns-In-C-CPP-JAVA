24f5d11526b7a440fb4a8a60dc35c6e160d9fc31
Raunak Gupta <111748076+Raunaksplanet@users.noreply.github.com>
2023-07-12 21:04:35 +0530

    Update All cpp questions.cpp

--- Cpp  Q & A/All cpp questions.cpp
+++ Cpp  Q & A/All cpp questions.cpp
@@ -22,4 +22,46 @@ int main()
     cout << (a/100)%10;
 }
 --------------------------------------------------------------------------------------------------------------------------------------
-Q3. 
+Q3. Write a program to input a number (minimum 5 Digit) from user and print this pattern.
+Eg n = 16241
+Pattern 
+* 
+* * * * * * 
+* * 
+* * * * 
+* 
+ans3.
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int n, len, r = 0, i, j, temp;
+    cout << "Enter a number: ";
+    cin >> n;
+    temp = n;
+
+    while (n > 0)
+    {
+        r = r * 10 + (n % 10);
+        n = n / 10;
+    }
+
+    temp = r;
+
+    for (len = 0; r > 0; (len++), (r = r / 10));
+
+    for (i = 0; i < len; i++)
+    {
+        r = temp % 10;
+        for (j = 1; j <= r; j++)
+        {
+            cout << "* ";
+        }
+        temp = temp / 10;
+        cout << "\n";
+    }
+}
+--------------------------------------------------------------------------------------------------------------------------------------
+Q4. 
