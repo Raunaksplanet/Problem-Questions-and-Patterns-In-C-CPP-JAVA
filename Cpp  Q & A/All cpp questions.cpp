33dc87834d56b959fad88007dcf765fd6049b3b8
Raunak Gupta <111748076+Raunaksplanet@users.noreply.github.com>
2023-08-04 07:51:47 +0530

    Update All cpp questions.cpp

--- Cpp  Q & A/All cpp questions.cpp
+++ Cpp  Q & A/All cpp questions.cpp
@@ -202,4 +202,27 @@ int nn;
 
     cout << "\n\n";
 --------------------------------------------------------------------------------------------------------------------------------------
-Q10.
+Q10. Write a program to input money amount from user and break down it in Indian currency system, for Eg:- if amount = 2375, breakdown = 2000  100  100  100  50  20  5
+ans10. 
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int amount;
+    int arr[9] = {2000,500, 100, 50, 20, 10, 5, 2, 1};
+    cout << "Enter a amount: ";
+    cin >> amount; 
+
+    cout << "\nBrakdown will be\n";
+    for (int i = 0; i < 9; i++)
+    {
+        while(amount>=arr[i])
+        {
+            cout << arr[i] << "  ";
+            amount = amount - arr[i];
+        } 
+    }
+}
+--------------------------------------------------------------------------------------------------------------------------------------
+Q11. 
