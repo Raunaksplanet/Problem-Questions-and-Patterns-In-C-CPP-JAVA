2cbe68f38bb639b453b613f02906fd7f86497d7c
Raunak Gupta <111748076+Raunaksplanet@users.noreply.github.com>
2023-08-30 23:39:07 +0530

    Add files via upload

---
+++ Java Programming Questions/Q1_ReverseNumber.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Q1_ReverseNumber{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+
+        int n;
+        System.out.print("Enter Number: ");
+        n = sc.nextInt();
+
+        String str = Integer.toString(n);
+        StringBuilder sb = new StringBuilder(str);
+        System.out.println("Reversed Number: " + sb.reverse());
+    }
+}
