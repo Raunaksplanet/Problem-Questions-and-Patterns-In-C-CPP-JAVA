7809eb3392c9655ebcf524e24fd161e58b20dae1
Raunak Gupta <111748076+Raunaksplanet@users.noreply.github.com>
2023-08-30 23:43:42 +0530

    Update Q1_ReverseNumber.java

--- Java Programming Questions/Q1_ReverseNumber.java
+++ Java Programming Questions/Q1_ReverseNumber.java
@@ -1,5 +1,10 @@
-import java.util.Scanner;
+/*
+ q1. 
+ Write a program to enter a number from user and reverse it without 
+ using loops and operators.
+ */
 
+import java.util.Scanner;
 public class Q1_ReverseNumber{
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
