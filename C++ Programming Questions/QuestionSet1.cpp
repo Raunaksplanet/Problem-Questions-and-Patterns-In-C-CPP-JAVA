4a7cf6e7ad46791511d52dad8bdb2ba18f2c7035
Raunak Gupta <111748076+Raunaksplanet@users.noreply.github.com>
2023-09-01 22:27:17 +0530

    Update QuestionSet1.cpp

--- C++ Programming Questions/QuestionSet1.cpp
+++ C++ Programming Questions/QuestionSet1.cpp
@@ -428,4 +428,19 @@ int main()
     cout << "Number of elements: " << n;
 }
 --------------------------------------------------------------------------------------------------------------------------------------
-Q17.
+Q17.Reverse array without using loop.
+Ans17.
+#include<iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    int arr[] = {1,2,3,4,5,6,7,8,9,10};
+
+    reverse(arr,arr+10);
+    for (int i = 0; i < 10; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
