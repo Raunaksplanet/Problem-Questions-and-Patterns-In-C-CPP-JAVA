53bd52725568bf08d1e890c98796c064bd427a1b
Raunak Gupta <111748076+Raunaksplanet@users.noreply.github.com>
2023-09-05 00:02:29 +0530

    Update QuestionSet2.cpp

--- C++ Programming Questions/QuestionSet2.cpp
+++ C++ Programming Questions/QuestionSet2.cpp
@@ -8,5 +8,37 @@ int main()
     cout << __FILE__;
 }
 --------------------------------------------------------------------------------------------------------------------------------------
-Q22.
+Q22.Write a program to print source code in output.
+ans22.
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main() {
+    ifstream sourceFile(__FILE__);
+
+    string line;
+    while (getline(sourceFile, line)) {
+        cout << line << endl;
+    }
+}
+
+// OR Alternative Code
+
+#include<stdio.h>
+
+int main()
+{
+    char c;
+    FILE *fp = fopen(_FILE_, "r");
+  
+    do
+    {
+        c = fgetc(fp);
+        putchar(c);
+    }
+    while (c != EOF);
+}
 ------------------------------------------------------------------------------------------------------------------------------------
+Q23.
